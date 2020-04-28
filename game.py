152029d98519b62404b2a71aaecf8b23a1f0de32
Erica Chesley <erchesley@gmail.com>
2020-04-28 20:01:22 +0000

    Pick number and invite guesses

--- game.py
+++ game.py
@@ -1,5 +1,11 @@
 """A number-guessing game."""
 
 # Put your code here
+from random import randint
+
 print("Welcome to my number guessing game!")
 name = input("What's your name? ")
+
+number = randint(1, 100)
+print("I'm thinking of a number between 1 and 100. Can you guess it?")
+print(number)
