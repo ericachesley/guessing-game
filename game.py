1b57532ad0836e6bfe1649efe000390408b15a6b
Erica Chesley <erchesley@gmail.com>
2020-04-28 22:20:42 +0000

    Full basic gmn

--- game.py
+++ game.py
@@ -7,5 +7,17 @@ print("Welcome to my number guessing game!")
 name = input("What's your name? ")
 
 number = randint(1, 100)
-print("I'm thinking of a number between 1 and 100. Can you guess it?")
-print(number)
+print(f"Ok, {name}, I'm thinking of a number between 1 and 100. Can you guess it?")
+
+count = 0
+
+while True:
+    guess = int(input("What's my number? "))
+    count += 1
+    if guess == number:
+        print (f"You guessed my number in {count} tries, {name}!")
+        break
+    elif guess < number:
+        print ("Too low.")
+    else:
+        print ("Too high.")
